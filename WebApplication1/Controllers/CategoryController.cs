24d3b31a04d74b472bc1c51cd0015f243cc7661b
Mohamed Gohar <mymohamedgohar@gmail.com>
2022-08-13 23:16:11 +0200

    Add project files.

---
+++ WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoriesList = _db.Categories;
+
+            return View(categoriesList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "The DisplayOrder Can't Exactly Match The Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Added Successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFormDb = _db.Categories.Find(id);
+
+            if (categoryFormDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFormDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "The DisplayOrder Can't Exactly Match The Name");
+            }
+    
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryD = _db.Categories.Find(id);
+            if (categoryD == null)
+            {
+                return NotFound();
+            }
+            return View(categoryD);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category obj)
+        {
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
